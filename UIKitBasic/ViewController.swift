bff07e787d7b454a8e48e3a0812479e9d9c69078
Pan <pannguyen@motorist.vn>
2026-01-08 09:25:14 +0700

    Initial Commit

---
+++ UIKitBasic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKitBasic
+//
+//  Created by Macbook on 08/01/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
